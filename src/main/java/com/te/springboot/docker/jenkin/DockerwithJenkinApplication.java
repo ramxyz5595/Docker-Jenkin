e32b8e08532007c069d12ecaea7d32e8192ad169
e <ramxyz5595@gmail.com>
2025-05-10 17:25:27 +0530

    first commit

---
+++ src/main/java/com/te/springboot/docker/jenkin/DockerwithJenkinApplication.java
@@ -0,0 +1,13 @@
+package com.te.springboot.docker.jenkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerwithJenkinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerwithJenkinApplication.class, args);
+	}
+
+}
